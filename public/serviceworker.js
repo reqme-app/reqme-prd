063c73e07f17eac24b05a262570d29705ef30609
Reqme (via WeWeb) <reqme.info@gmail.com>
2026-01-04 18:26:40 +0900

    v578 - ver1.0.8

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 577;
+const version = 578;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
